d309f056a0e5f0f9caf7f0d7abd5eb0663c7d1e9
Vinayak S <vinayaksukhalal@gmail.com>
2023-12-10 18:07:56 +0530

    Updated

---
+++ SealedClass.kt
@@ -0,0 +1,16 @@
+sealed class Hello{
+    class Hi:Hello(){
+        fun ggggg(){
+            print("hhhhhhhhhhhhhhhhhhhhh")
+        }
+        
+    }
+}
+
+
+
+fun main()
+{
+    var hee=Hello.Hi()
+    hee.ggggg()
+}
